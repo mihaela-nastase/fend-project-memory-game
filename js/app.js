13ead097ece2b554c1fb53d3e6ecb791989e39d5
Mihaela-Cristina Nastase <mihaela.nastase93@yahoo.ro>
2018-10-28 03:42:50 +0200

    Flip and display the clicked cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-/*
- * Create a list that holds all of your cards
- */
+// List that holds all of the cards
+const cards = [...document.getElementsByClassName("card")];
+const deck = document.querySelector(".deck");
 
+//list holding the "open" cards
+let openedCards = [];
 
 /*
  * Display the cards on the page
@@ -26,11 +28,29 @@ function shuffle(array) {
 }
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
+// set up the event listener for the cards to display them and add them to a list of "open" cards
+for (card of cards) {
+	card.addEventListener("click", displayCard);
+	card.addEventListener("click", add2OpenedCards);
+}
+
+
+//If a card is clicked, display the card's symbol
+function displayCard() {
+	//flip the card
+	this.classList.toggle("flipped");
+	//prevent double clicking of the same card
+	this.style.pointerEvents = "none";
+}
+
+
+function add2OpenedCards() {
+
+	//if the card is clicked, add the card to a list of "open" cards
+	openedCards.push(this);
+}
+
+/*  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
